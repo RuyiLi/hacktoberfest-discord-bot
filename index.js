3eb56892663f89a95fcf982d7bea82993ad041d2
Ruyi Li <ruyili2002@gmail.com>
2018-10-01 09:25:14 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client } = require('discord.js');
+const client = new Client();
+const prefix = 'hack!';
+
+client.on('ready', () => console.log('Bot has logged in!'));
+
+client.on('message', async (msg) => {
+  if(msg.author.bot || !msg.content.startsWith(prefix)) return;
+  let args = msg.content.split(' ');
+  const command = args.shift().slice(prefix.length);
+  //Add your commands here. Good command handlers are overrated :POGGERS:
+  
+  if(command === 'ping'){
+    const m = await msg.channel.send('Pinging...');
+    m.edit(`Ponggers! Client ping: \`${m.createdTimestamp - msg.createdTimestamp}\`ms. Heartbeat ping: \`${client.ping}\`ms`);
+  }
+  
+})
+
+client.login(process.env.TOKEN);
