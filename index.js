d7181d06bb57676e977034fa53ec165976ca1b91
Martin Janeček <admin@ikw.cz>
2018-10-01 17:51:47 +0200

    add status command to fetch Hacktoberfest pullrequest count

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, Attachment } = require('discord.js');
+const https = require('https');
 const client = new Client();
 const prefix = 'hack!';
 
@@ -9,7 +10,7 @@ client.on('message', async (msg) => {
   let args = msg.content.split(' ');
   const command = args.shift().slice(prefix.length);
   //Add your commands here. Good command handlers are overrated :POGGERS:
-  
+
   if(command === 'ping'){
     const m = await msg.channel.send('Pinging...');
     m.edit(`Ponggers! Client ping: \`${m.createdTimestamp - msg.createdTimestamp}\`ms. Heartbeat ping: \`${client.ping}\`ms`);
@@ -18,6 +19,29 @@ client.on('message', async (msg) => {
   } else if(command === 'lul') {
     const attachment = new Attachment('https://ubisafe.org/images/lul-transparent-twitch-1.png');
     message.channel.send(attachment);
+  } else if(command === 'status') {
+    const m = await msg.channel.send('Querying GitHub for pull request count during Hacktoberfest...');
+    const username = args[0];
+    const apiUrl = `https://api.github.com/search/issues?q=-label:invalid+created:2018-09-30T10%3A00%3A00%2B00%3A00..2018-11-01T12%3A00%3A00%2B00%3A00+type:pr+is:public+author:${username}&per_page=300`;
+    https.get(apiUrl, {
+      headers: {
+        'User-Agent': 'Hacktoberfest bot <https://github.com/RuyiLi/hacktoberfest-discord-bot>',
+      },
+    }, resp => {
+      let data = '';
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+      resp.on('end', () => {
+        const result = JSON.parse(data);
+
+        if(result.errors) {
+          m.edit(`${username} not found :(`);
+        } else {
+          m.edit(`${username} made ${result.total_count}/5 pull requests during Hacktoberfest 2018`);
+        }
+      });
+    });
   }
 })
 
